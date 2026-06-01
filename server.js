2c1d5334892683cf132b57873b6f135bd4c32263
Sanji <your-email@gmail.com>
2026-06-01 10:18:53 +0530

    feat: add MongoDB database connection

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
 import "dotenv/config";
 import app from "./src/app.js";
+import connectDB from "./src/config/database.js";
+
 
 const port = process.env.PORT || 4000;
+
+await connectDB()
 app.listen(port, () => console.log("server is working....3000"));
